b90c89353315c8ce677b339ef1ad803289a0326c
T-Rzeznik <trzeznik29@gamil.com>
2024-07-16 19:47:52 -0400

    Added memory, README

---
+++ README.md
@@ -0,0 +1,9 @@
+GUITAR KNOWLEDGE BOT
+
+
+This project uses OPENAI API to create a chat bot.
+
+
+pip install openai
+
+Make sure to setup system enviroment variables for YOUR own API key.
--- source.py
+++ source.py
@@ -6,17 +6,19 @@ client = OpenAI(
     organization='org-RCXMfWloXmAr0B6q9cideLet',
     project='proj_rbyAMmXk7nEiONsg85H969aH',
 )
-
+conversation = [] #Updated array to keep track of conversation
 
 def get_gpt_response(user_input):
     message = {
         "role": "user",
         "content": user_input
     }
+    conversation.append(message)
     response = client.chat.completions.create(
-        messages = [message],
+        messages = conversation,
         model = "gpt-3.5-turbo",
     )
+    conversation.append(response.choices[0].message)
     return response.choices[0].message.content
 
 def chat():
