a5f6c3435d20577bb153c7f9659dca2fed689106
T-Rzeznik <trzeznik29@gamil.com>
2024-07-17 17:12:10 -0400

    Created Interface, Setup bot to work with interface

--- BotSource.py
+++ BotSource.py
@@ -2,9 +2,7 @@ import os
 from openai import OpenAI
 
 client = OpenAI(
-    api_key = os.getenv("OPENAI_API_KEY"),
-    organization='org-RCXMfWloXmAr0B6q9cideLet',
-    project='proj_rbyAMmXk7nEiONsg85H969aH',
+    api_key = os.getenv("OPENAI_API_KEY")
 )
 conversation = [] #Updated array to keep track of conversation
 
@@ -20,15 +18,3 @@ def get_gpt_response(user_input):
     )
     conversation.append(response.choices[0].message)
     return response.choices[0].message.content
-
-def chat():
-    while True:
-        user_input = input("You: ")
-        if user_input == 'exit':
-          print("bye")
-          break
-        response = get_gpt_response(user_input)
-        print(f"Chatbot: {response}")
-
-if __name__ == "__main__":
-    chat()
